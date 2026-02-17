93d82d6137ec668ac5989d0dba70a90abc3d4dc3
mrjrask <96755551+mrjrask@users.noreply.github.com>
2026-02-16 21:48:50 -0600

    Keep desktop cursor hidden automatically on startup

--- tests/test_utils_kernel_cursor_wiggle.py
+++ tests/test_utils_kernel_cursor_wiggle.py
@@ -28,6 +28,23 @@ class _FakePygame:
     def __init__(self):
         self.mouse = _FakeMouse()
         self.event = _FakeEvent()
+        self.display = _FakeDisplay()
+
+
+class _FakeSurface:
+    def __init__(self, size=(320, 240)):
+        self._size = size
+
+    def get_size(self):
+        return self._size
+
+
+class _FakeDisplay:
+    def __init__(self):
+        self.surface = _FakeSurface()
+
+    def get_surface(self):
+        return self.surface
 
 
 def test_wiggle_mouse_cursor_moves_and_restores_pointer():
@@ -53,10 +70,9 @@ def test_schedule_mouse_cursor_wiggle_starts_daemon_timer(monkeypatch):
     captured = {}
 
     class _FakeTimer:
-        def __init__(self, interval, callback, kwargs):
+        def __init__(self, interval, callback):
             captured["interval"] = interval
             captured["callback"] = callback
-            captured["kwargs"] = kwargs
             self.daemon = False
             self.started = False
 
@@ -71,8 +87,8 @@ def test_schedule_mouse_cursor_wiggle_starts_daemon_timer(monkeypatch):
     assert isinstance(timer, _FakeTimer)
     assert timer.daemon is True
     assert captured["interval"] == 30
-    assert captured["callback"] is utils._wiggle_mouse_cursor
-    assert captured["kwargs"] == {"pygame_module": fake_pygame, "distance": 1}
+    captured["callback"]()
+    assert fake_pygame.mouse.moves == [(101, 50), (100, 50)]
     assert captured["started"] is True
 
 
@@ -83,3 +99,34 @@ def test_schedule_mouse_cursor_wiggle_noops_for_nonpositive_delay(monkeypatch):
     monkeypatch.setattr(utils.threading, "Timer", _raise_if_called)
 
     assert utils._schedule_mouse_cursor_wiggle(_FakePygame(), delay_seconds=0) is None
+
+
+def test_park_mouse_cursor_moves_pointer_to_bottom_right():
+    fake_pygame = _FakePygame()
+
+    utils._park_mouse_cursor(fake_pygame)
+
+    assert fake_pygame.mouse.moves[-1] == (319, 239)
+    assert fake_pygame.event.pump_count == 1
+
+
+def test_schedule_mouse_cursor_wiggle_repeat_rearms_timer(monkeypatch):
+    fake_pygame = _FakePygame()
+    captured_callbacks = []
+
+    class _FakeTimer:
+        def __init__(self, _interval, callback):
+            captured_callbacks.append(callback)
+            self.daemon = False
+
+        def start(self):
+            return None
+
+    monkeypatch.setattr(utils.threading, "Timer", _FakeTimer)
+
+    timer = utils._schedule_mouse_cursor_wiggle(fake_pygame, delay_seconds=30, repeat=True)
+    assert isinstance(timer, _FakeTimer)
+
+    captured_callbacks[0]()
+    assert fake_pygame.mouse.moves == [(101, 50), (100, 50)]
+    assert len(captured_callbacks) == 2
